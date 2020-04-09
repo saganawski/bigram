ab6c0f66e74197d7cc065ec433b990b9aa77a3c0
Ken Saganski <Ken.saganski@3won.com>
2020-04-08 21:05:30 -0500

    move file location inside of argument check, Update error message

--- src/main/java/com/saganski/bigram/BigramApplication.java
+++ src/main/java/com/saganski/bigram/BigramApplication.java
@@ -14,9 +14,10 @@ public class BigramApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BigramApplication.class, args);
-		final String fileLocation = args[0];
 		
 		if(BigramUtil.argumentCheck(args)) {
+			final String fileLocation = args[0];
+			
 			final List<String> parsedFileLines = BigramUtil.parseFileLinesToStringList(fileLocation);
 			
 			final List<Map<String, Integer>> bigramResult = new ArrayList<>();
@@ -32,14 +33,16 @@ public class BigramApplication {
 			bigramResult.forEach( b -> System.out.println(b));
 			
 			BigramUtil.writeFile(bigramResult);
-	
+			
 			
 		}else {
+			System.out.println("*****************************************************************************************************");
+			System.out.println("******************** ERROR ********************");
 			final String exampleFileLocation = "C:\\Users\\ken.saganski\\Desktop\\test\\TestFile.txt";
-			System.out.println("Please add location of file to parse. For ex: java Bigram " + exampleFileLocation);
+			System.out.println("This application takes one one(1) argument run, and that is the file loction to parse. For ex: java Bigram " + exampleFileLocation +"\n");
+			System.out.println("*****************************************************************************************************");
 		}
-		
-		
+			
 	}
 	
 }
